b8e1ad7b62b711ff9db2757f76da6897d28d7cf7
NkAndre <andre270708@gmail.com>
2026-04-20 19:23:58 -0300

    feat:exercicios numero1 finalizado

--- aulasDS/moedasConvertidas.py
+++ aulasDS/moedasConvertidas.py
@@ -1,7 +1,18 @@
 import requests
 
-nomeMoeda = int (input(" [1] - USD\n [2] - BRL\n [3] - EURO\n [4] - ARS"))
+link = "https://economia.awesomeapi.com.br/last/USD-BRL,ARS-BRL,ETH-BRL"
 
-valorMoeda = float(input("Insira a moeda : "))
+# 2. requisao e transformacao
+requisicao = requests.get(link)
+dicMoedas = requisicao.json()
 
-link = f""
+
+valor_dolar = dicMoedas['USDBRL']['bid']
+valor_peso = dicMoedas['ARSBRL']['bid']
+valor_ethereum = dicMoedas['ETHBRL']['bid']
+
+# 4. exbicao do resultadp
+print("=== COTAÇÕES DE HOJE ===")
+print(f"Valor do Dólar americano hoje: R$ {valor_dolar}")
+print(f"Valor do Peso argentino hoje: R$ {valor_peso}")
+print(f"Valor do Ethereum hoje: R$ {valor_ethereum}")
